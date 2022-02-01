381fb142b03bbe92949e2bb873d01f7dfc4d86b5
ningalu <thomasholster2012@gmail.com>
2022-02-01 23:17:36 +1300

    test game end when states are depleted

--- src/Framework/Core/Game.cpp
+++ src/Framework/Core/Game.cpp
@@ -7,9 +7,9 @@ namespace nge {
         graphics_ = std::make_shared<Graphics>();
         
         state_manager_ = std::make_shared<StateManager>();
-        state_manager_->Advance(new State(graphics_));
+        state_manager_->Advance(std::shared_ptr<State>(new State(state_manager_, graphics_)));
         std::cout << "Initial State: " << state_manager_->GetCurrentState() << std::endl;
-        state_manager_->Advance(new State(graphics_));
+        state_manager_->Advance(std::shared_ptr<State>(new State(state_manager_, graphics_)));
         std::cout << "Initial State: " << state_manager_->GetCurrentState() << std::endl;
         input_ = std::make_shared<Input>();
         test = graphics_->LoadTexture("resources/stewie.jpg");
@@ -18,21 +18,9 @@ namespace nge {
     void Game::Start() {
         running_ = true;
 
-        while (running_) {
+        while (running_ && !state_manager_->IsEmpty()) {
             if (tick_timer_.GetElapsedTime() > (1.0 / 1000)) {
-                input_->UpdatePrevInput();
-                while (SDL_PollEvent(&event_buffer_)) {
-                    switch (event_buffer_.type) {
-                        case SDL_QUIT:
-                            std::cout << "SDL_QUIT" << std::endl;
-                            running_ = false;
-                            break;
-                    }  
-                }
-                input_->Update();
-                if (input_->KeyPressed(SDL_SCANCODE_A)) {
-                    std::cout << "A" << std::endl;
-                }
+                ProcessInput();
                 Tick();
                 tick_timer_.Reset();
             }
@@ -42,7 +30,7 @@ namespace nge {
             }
             if (state_timer_.GetElapsedTime() > 2) {
                 state_manager_->Return();
-                state_timer_.Stop();
+                state_timer_.Reset();
             }
         }
     }
@@ -63,6 +51,19 @@ namespace nge {
         graphics_->Clear();
         state_manager_->GetCurrentState()->Draw();
         graphics_->Present();
+    }
+
+    void Game::ProcessInput() {
+        state_manager_->GetCurrentState()->UpdatePreviousInput();
+        while (SDL_PollEvent(&event_buffer_)) {
+            switch (event_buffer_.type) {
+                case SDL_QUIT:
+                    std::cout << "SDL_QUIT" << std::endl;
+                    running_ = false;
+                    break;
+            }  
+        }
+        state_manager_->GetCurrentState()->UpdateCurrentInput();
     }
 
     Game::~Game() {
