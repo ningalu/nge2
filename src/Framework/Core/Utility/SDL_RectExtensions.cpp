6062f6ba9f74a84bb92ad0eebeb441fb8eea0f61
ningalu <thomasholster2012@gmail.com>
2022-02-07 14:17:12 +1300

    overload for cout

--- src/Framework/Core/Utility/SDL_RectExtensions.cpp
+++ src/Framework/Core/Utility/SDL_RectExtensions.cpp
@@ -6,3 +6,15 @@ bool operator==(SDL_Rect lhs, SDL_Rect rhs) {
     && (lhs.w == rhs.w)
     && (lhs.h == rhs.h);
 }
+
+std::ostream& operator<<(std::ostream& os, const SDL_Rect& rect) {
+    os << "x: " 
+    << rect.x 
+    << "\ny: " 
+    << rect.y 
+    << "\nw: " 
+    << rect.w 
+    << "\nh: " 
+    << rect.h;
+    return os;
+}
--- src/Framework/Core/Utility/SDL_RectExtensions.h
+++ src/Framework/Core/Utility/SDL_RectExtensions.h
@@ -2,7 +2,9 @@
 #define _SDL_RECTEXTENSIONS_H
 
 #include "SDL2/SDL.h"
+#include <iostream>
 
 bool operator==(SDL_Rect lhs, SDL_Rect rhs);
+std::ostream& operator<<(std::ostream& os, const SDL_Rect& rect);
 
 #endif
